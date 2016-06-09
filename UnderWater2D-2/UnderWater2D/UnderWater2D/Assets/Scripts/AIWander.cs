537cf56f1ed63527c553cadbdff4d0ad15feb445
Marlee Finn <Marlee Finn>
2016-06-09 16:51:20 -0400

    no message

---
+++ UnderWater2D-2/UnderWater2D/UnderWater2D/Assets/Scripts/AIWander.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIWander : MonoBehaviour
+{ 
+    public Rigidbody2D loneWanderer;
+    private int directionPicker;
+    public float timer = 0.0f; // how long it will go in the direction
+    public float thrust = 5.0f;
+
+    void Start()
+    {
+        loneWanderer = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (timer <= Time.time)
+        {
+
+            timer = Time.time + Random.Range(4, 10);
+            //timer = Time.time + 4.0f;
+            directionPicker = Random.Range(1, 5);
+            Direction();
+        }
+    }
+
+
+
+    void Direction()
+    {
+        if(directionPicker == 1)
+        {
+            loneWanderer.AddForce(-transform.up * thrust);
+        }
+       else if (directionPicker == 2)
+        {
+            loneWanderer.AddForce(transform.up * thrust);
+        }
+        else if (directionPicker == 3)
+        {
+            loneWanderer.AddForce(transform.right * thrust);
+        }
+        else if (directionPicker == 4)
+        {
+            loneWanderer.AddForce(-transform.right * thrust);
+        }
+    }
+
+}
+
---
+++ UnderWater2D-2/UnderWater2D/UnderWater2D/Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTransition : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private GameObject Target;
+    [SerializeField]
+    private GameObject Camera;
+    	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, Target.transform.position, step);
+        Camera.transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y, -10);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Target = col.gameObject;
+    }
+
+}

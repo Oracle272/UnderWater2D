29413ccee4449efcde855888910672b6ce12d85e
Marlee Finn <Marlee Finn>
2016-07-11 11:46:07 -0400

    updated bag ui

--- UnderWater2D-2/UnderWater2D/UnderWater2D/Assets/Scripts/Inventory.cs
+++ UnderWater2D-2/UnderWater2D/UnderWater2D/Assets/Scripts/Inventory.cs
@@ -13,6 +13,15 @@ public class Inventory : MonoBehaviour
     public GameObject BowButton;
     public GameObject Mace;
     public GameObject Bow;
+    public GameObject bagimage;
+    public GameObject maceCheck;
+    public GameObject bowCheck;
+    public GameObject macePic;
+    public GameObject bowPic;
+    public bool paused;
+    bool MaceEquiped;
+    bool BowEquipped;
+
     // Use this for initialization
     void Start()
     {
@@ -21,33 +30,67 @@ public class Inventory : MonoBehaviour
         Bow.SetActive(false);
         BowButton.SetActive(false);
         MaceButton.SetActive(false);
-
-
+        maceCheck.SetActive(false);
+        macePic.SetActive(false);
+        bowCheck.SetActive(false);
+        bowPic.SetActive(false);
+        bagimage.SetActive(false);
+        paused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            Time.timeScale = 1;
+        }
+            
     }
 
     public void bag() 
     {
+        paused = !paused;
         //BowButton.SetActive(true);
         //MaceButton.SetActive(true);
         BowButton.SetActive(!BowButton.activeSelf);
         MaceButton.SetActive(!MaceButton.activeSelf);
+        bagimage.SetActive(!bagimage.activeSelf);
+        if(MaceEquiped == true)
+        {
+            maceCheck.SetActive(!maceCheck.activeSelf);
+            BowEquipped = false;
+        }
+        if(BowEquipped == true)
+        {
+            bowCheck.SetActive(!bowCheck.activeSelf);
+            MaceEquiped = false;
+        }
+        //maceCheck.SetActive(!maceCheck.activeSelf);
+        //macePic.SetActive(!macePic.activeSelf);
+        //bowCheck.SetActive(!bowCheck.activeSelf);
+        //bowPic.SetActive(!bowPic.activeSelf);
     }
 
     public void SelectMace()
     {
         Bow.SetActive(false);
         Mace.SetActive(true);
+        maceCheck.SetActive(true);
+        bowCheck.SetActive(false);
+        MaceEquiped = true;
     }
 
     public void SelectBow()
     {
         Bow.SetActive(true);
         Mace.SetActive(false);
+        maceCheck.SetActive(false);
+        bowCheck.SetActive(true);
+        BowEquipped = true;
     }
 }
